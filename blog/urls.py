8519d11f897df83abc0ec864ce987e924e6d531b
Yosh Krupnick <yehohsua.krupnick@gmail.com>
2015-11-01 00:26:59 -0400

    add publish button, url and view to use model.Post.publish method

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -30,3 +30,8 @@ def post_new(request):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
